8623f2ee72ca96eec1536732feb7dceacca87192
okapetanios <ericegan@live.com>
2019-10-30 16:59:05 -0700

    added enableswagger2 annotation to swagdocconfig.java

--- src/main/java/com/example/postgresdemo/config/SwaggerDocumentationConfig.java
+++ src/main/java/com/example/postgresdemo/config/SwaggerDocumentationConfig.java
@@ -9,6 +9,7 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen",
     date = "2019-10-11T13:23:56.479615-07:00[America/Los_Angeles]")
