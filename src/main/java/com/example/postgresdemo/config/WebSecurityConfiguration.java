140437e2fd2327936b479ef9055a8d435f6d5321
okapetanios <ericegan@live.com>
2019-10-30 17:57:05 -0700

    added @enableswagger2 to homecontroller and moved files back in from testest

--- src/main/java/com/example/postgresdemo/config/WebSecurityConfiguration.java
+++ src/main/java/com/example/postgresdemo/config/WebSecurityConfiguration.java
@@ -8,6 +8,15 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import com.appsdeveloperblog.app.ws.service.UserService;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 @EnableSwagger2
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
